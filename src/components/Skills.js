ab5d502b7b4ca4b1de5ae6b6708931b821998d32
Seaf Aliyan <codo47@gmail.com>
2022-08-29 18:17:17 +0300

    added skills section

---
+++ src/components/Skills.js
@@ -0,0 +1,77 @@
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png";
+import { Container, Row, Col } from "react-bootstrap";
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <section className="skills" id="skills">
+      <Container>
+        <Row className="row">
+          <Col className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+                modi quasi temporibus ex odit iste beatae! Id error minus quam
+                veritatis odit, quis earum porro nisi molestias voluptatum qui
+                veniam.
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="owl-carousel owl-theme skill-slider"
+              >
+                <div className="item">
+                  <img src={meter1} alt="image" />
+                  <h5>Web Development</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="image" />
+                  <h5>Data Science</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="image" />
+                  <h5>UX/UI</h5>
+                </div>
+                <div className="item">
+                  <img src={meter3} alt="image" />
+                  <h5>Project Management</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="image" />
+                  <h5>Mobile Development</h5>
+                </div>
+              </Carousel>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-left" src={colorSharp} />
+    </section>
+  );
+};
