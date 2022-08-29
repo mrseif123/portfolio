cb82080b67e932f3d69f2b2ed63c5a9b245338bc
Seaf Aliyan <codo47@gmail.com>
2022-08-29 17:58:18 +0300

    added banner section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,27 +1,97 @@
 import { Container, Row, Col } from "react-bootstrap";
+import headerImage from "../assets/img/header-img.svg";
+import { useState, useEffect } from "react";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const toRotate = ["Developer", "Designer", "Freelancer"];
+  const [text, setText] = useState("");
+  const period = 2000;
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+    setText(updatedText);
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+      setIsDeleting(true);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
   return (
     <section className="banner" id="home">
       <Container>
-        <Row className="align-items-center">
+        <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
-            <h1>
-              {`Hey I'm Seaf`}
-              <span className="tagline">Hello, I'm</span>
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
-              modi sunt quidem, quasi minima magnam illum soluta doloremque
-              repudiandae enim mollitia facere quam omnis! Qui rerum maxime eos
-              amet quas?
-            </p>
-            <button onClick={() => console.log("connect")}>
-              Let's connect!
-            </button>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>
+                    {`Hi! I'm Seaf `}
+                    <span
+                      className="txt-rotate"
+                      dataPeriod="1000"
+                      data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'
+                    >
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </p>
+                  <button onClick={() => console.log("connect")}>
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src={} alt="Header image"/>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src={headerImage} alt="Header Img" />
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
